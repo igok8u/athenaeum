59e5e2fdd34d7c09c3323cfe3f985f79eea8a25d
Ihar Kuzniatsou <ihar.kuzniatsou@check24.de>
2024-03-18 13:06:33 +0100

    Init project

---
+++ backend/src/main/java/com/ik8u/athenaeum/backend/BackendApplication.java
@@ -0,0 +1,13 @@
+package com.ik8u.athenaeum.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendApplication.class, args);
+	}
+
+}
